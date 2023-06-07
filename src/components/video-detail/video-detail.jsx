f477c810645629080de013af95d48d5adc76930e
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-06-07 18:25:41 +0500

    Suggested video

--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ApiService } from "../../service/api.service";
 import ReactPlayer from "react-player";
+import { GrView } from "react-icons/gr";
+import { FcLike } from "react-icons/fc";
+import { FcComments } from "react-icons/fc";
+import { Videos } from "../";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
-
+  const [relatedVideo, setRelatedVideo] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -14,8 +18,12 @@ const VideoDetail = () => {
         const data = await ApiService.fetching(
           `videos?part=snippet,statistics&id=${id}`
         );
-
         setVideoDetail(data?.items[0]);
+
+        const relatedData = await ApiService.fetching(
+          `search?parent=snippet&relatedToVideoId=${id}&type=video`
+        );
+        setRelatedVideo(relatedData.items);
       } catch (error) {
         console.log(error);
       }
@@ -30,8 +38,8 @@ const VideoDetail = () => {
   console.log(videoDetail?.snippet);
   return (
     <div className="min-h-[90vh] mb-10">
-      <div className="flex">
-        <div className="w-[75%] ml-3">
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-[75%] ml-3">
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${id}`}
             className="react-player"
@@ -40,10 +48,27 @@ const VideoDetail = () => {
 
           <div className="flex items-center mt-3">
             <h2 className="font-bold">{videoDetail?.snippet?.channelTitle}</h2>
-            <div className="flex items-center">
-              <div className="flex items-center">
-                {parseInt(videoDetail?.statistics?.viewCount).toLocaleString()}{" "}
+            <div className="flex items-center gap-3 ml-5">
+              <div className="flex items-center gap-1">
+                <GrView />
+                {parseInt(
+                  videoDetail?.statistics?.viewCount
+                ).toLocaleString()}{" "}
                 veiws
+              </div>
+              <div className="flex items-center gap-1">
+                <FcLike />
+                {parseInt(
+                  videoDetail?.statistics?.likeCount
+                ).toLocaleString()}{" "}
+                likes
+              </div>
+              <div className="flex items-center gap-1">
+                <FcComments />
+                {parseInt(
+                  videoDetail?.statistics?.commentCount
+                ).toLocaleString()}{" "}
+                comment
               </div>
             </div>
           </div>
@@ -60,7 +85,9 @@ const VideoDetail = () => {
           <h1 className="mt-3 font-bold">{videoDetail?.snippet?.title}</h1>
           <p className="mt-5">{videoDetail?.snippet?.description}</p>
         </div>
-        <div className="w-[25%]">Suggested Videos</div>
+        <div className="w-full md:w-[25%]">
+          <Videos videos={relatedVideo} />
+        </div>
       </div>
     </div>
   );
