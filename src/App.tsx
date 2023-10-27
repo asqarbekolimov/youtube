fc4ff3236706882d4ba39c16cc27a1ea96a977c6
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-10-27 10:13:18 +0500

    feat: add shorts page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import Search from './pages/search';
+import Shorts from './pages/shorts';
 
 const App = () => {
   return (
     <Box>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/shorts" element={<Shorts />} />
         <Route path="/search/:id" element={<Search />} />
       </Routes>
     </Box>
---
+++ src/pages/shorts.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import Layout from '../layout/layout';
+import { Grid, GridItem, Skeleton } from '@chakra-ui/react';
+import { VideoType } from '../interfaces/video.interface';
+import VideoCard from '../components/card/card';
+import { ApiService } from '../service/api.service';
+
+const Shorts = () => {
+  const [videos, setVideos] = useState<VideoType[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getData = async () => {
+      try {
+        const data = await ApiService.fetching(`search?part=snippet&q=shorts`);
+        setVideos(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(true);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <Layout>
+      <Grid
+        templateColumns={{
+          md: 'repeat(2, 1fr)',
+          lg: 'repeat(3, 1fr)',
+        }}
+        pt={10}
+        gap={5}
+      >
+        {videos?.map((item, idx) => (
+          <Skeleton key={idx} isLoaded={!isLoading}>
+            <GridItem key={idx}>
+              <VideoCard video={item.snippet} isLoading={!isLoading} />
+            </GridItem>
+          </Skeleton>
+        ))}
+      </Grid>
+    </Layout>
+  );
+};
+
+export default Shorts;
